a9fbb324f1135b943af1d62ed63b7754cbb64c5d
ogola jeremy <ogolajeremy601@gmail.com>
2025-03-27 04:34:50 +0300

    Updates routes

--- routes/api.php
+++ routes/api.php
@@ -6,31 +6,36 @@ use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 use App\Http\Controllers\ClothingItemController;
 use App\Http\Controllers\CategoryController;
 
-
-// Public routes
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::get('/api', function () {
-    return response()->json(['message' => 'API Working']);
-});
-
-// Protected routes that require authentication
-Route::middleware([EnsureFrontendRequestsAreStateful::class, 'auth:sanctum'])->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
+// Prefix all routes with /api
+Route::prefix('api')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/', function () {
+        return response()->json(['message' => 'API Working']);
     });
 
+    Route::middleware([EnsureFrontendRequestsAreStateful::class, 'auth:sanctum'])->group(function () {
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+    });
+
+    Route::post('/clothing-items', [ClothingItemController::class, 'store']);
+    Route::get('/clothing-items', [ClothingItemController::class, 'index']);
+    Route::get('/clothing-items/{id}', [ClothingItemController::class, 'show']);
+    Route::put('/clothing-items/{id}', [ClothingItemController::class, 'update']);
+    Route::delete('/clothing-items/{id}', [ClothingItemController::class, 'destroy']);
+    Route::get('/categories/{id}', [CategoryController::class, 'show']);
+    Route::post('/categories', [CategoryController::class, 'store']);
+    Route::put('/categories/{id}', [CategoryController::class, 'update']);
+    Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/categories', [CategoryController::class, 'index']);
 });
-Route::post('clothing-items', [ClothingItemController::class, 'store']);
-Route::get('clothing-items', [ClothingItemController::class, 'index']);
-Route::get('clothing-items/{id}', [ClothingItemController::class, 'show']);
-Route::put('clothing-items/{id}', [ClothingItemController::class, 'update']);
-Route::delete('clothing-items/{id}', [ClothingItemController::class, 'destroy']);
-Route::get('categories/{id}', [CategoryController::class, 'show']);
-Route::post('categories', [CategoryController::class, 'store']);
-Route::put('categories/{id}', [CategoryController::class, 'update']);
-Route::delete('categories/{id}', [CategoryController::class, 'destroy']);
-Route::post('/logout', [AuthController::class, 'logout']);
-Route::get('categories', [CategoryController::class, 'index']);
+
+// Add a root route for welcome or redirection
+Route::get('/', function () {
+    return response()->json(['message' => 'Welcome to the Wardrobe API']);
+});
 
 
