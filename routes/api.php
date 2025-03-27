7f36252d82d83ce86758c05d5d9b923773eb2f07
ogola jeremy <ogolajeremy601@gmail.com>
2025-03-27 23:57:18 +0300

    Updates Dockerfile

--- routes/api.php
+++ routes/api.php
@@ -8,31 +8,29 @@ use App\Http\Controllers\ClothingItemController;
 use App\Http\Controllers\CategoryController;
 
 // Prefix all routes with /api
-Route::prefix('api')->group(function () {
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::get('/', function () {
-        return response()->json(['message' => 'API Working']);
-    });
-
-    Route::middleware([EnsureFrontendRequestsAreStateful::class, 'auth:sanctum'])->group(function () {
-        Route::get('/user', function (Request $request) {
-            return $request->user();
-        });
-    });
-
-    Route::post('/clothing-items', [ClothingItemController::class, 'store']);
-    Route::get('/clothing-items', [ClothingItemController::class, 'index']);
-    Route::get('/clothing-items/{id}', [ClothingItemController::class, 'show']);
-    Route::put('/clothing-items/{id}', [ClothingItemController::class, 'update']);
-    Route::delete('/clothing-items/{id}', [ClothingItemController::class, 'destroy']);
-    Route::get('/categories/{id}', [CategoryController::class, 'show']);
-    Route::post('/categories', [CategoryController::class, 'store']);
-    Route::put('/categories/{id}', [CategoryController::class, 'update']);
-    Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/categories', [CategoryController::class, 'index']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/', function () {
+    return response()->json(['message' => 'API Working']);
 });
+
+Route::middleware([EnsureFrontendRequestsAreStateful::class, 'auth:sanctum'])->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
+
+Route::post('/clothing-items', [ClothingItemController::class, 'store']);
+Route::get('/clothing-items', [ClothingItemController::class, 'index']);
+Route::get('/clothing-items/{id}', [ClothingItemController::class, 'show']);
+Route::put('/clothing-items/{id}', [ClothingItemController::class, 'update']);
+Route::delete('/clothing-items/{id}', [ClothingItemController::class, 'destroy']);
+Route::get('/categories/{id}', [CategoryController::class, 'show']);
+Route::post('/categories', [CategoryController::class, 'store']);
+Route::put('/categories/{id}', [CategoryController::class, 'update']);
+Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+Route::post('/logout', [AuthController::class, 'logout']);
+Route::get('/categories', [CategoryController::class, 'index']);
 
 // Add a root route for welcome or redirection
 Route::get('/', function () {
